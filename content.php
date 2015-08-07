2b3e4a898af3ca4e67eccdc71f1e534ff96e7499
Gamrok <Gamrok4@users.noreply.github.com>
2015-08-07 09:06:31 +0200

    Update content.php

--- content.php
+++ content.php
@@ -7,7 +7,7 @@
 <div class="post" id="post-<?php the_ID(); ?>">
 		
 	<div class="meta_top">
-		<h2><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2><span class="date"><?php echo bm_human_time_diff_enhanced(); ?></span>
+		<h2><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2><span class="separator">&nbsp;&#183;&nbsp;</span><span class="date"><?php echo bm_human_time_diff_enhanced(); ?></span>
 	</div><!-- .meta_top -->
 
 	<div class="entry">
@@ -15,7 +15,7 @@
 	</div><!-- .entry -->
 
 	<div class="meta_bottom">
-		<span class="post_tags"><?php the_tags('', '', ''); ?></span><span class="separator"> · </span><?php comments_popup_link( '0 notes', '1 note', '% notes', 'notes-link', '0 notes'); ?>
+		<span class="post_tags"><?php the_tags( '<ul><li>', '</li><li>', '</li></ul>' ); ?></span>
 	</div><!-- .meta_bottom -->
 
 </div><!-- .post -->
